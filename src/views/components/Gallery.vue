2c4abcfcff177a34f3f37f2b3ad2aa44c1f2b6d5
Seigi Takada <seiji.takada@medixserve.com>
2019-08-01 14:12:03 +0800

    Updated gallery

--- src/views/components/Gallery.vue
+++ src/views/components/Gallery.vue
@@ -3,64 +3,15 @@
     <div class="container">
       <div class="row">
         <div class="col-lg-12 col-md-12 text-center" data-aos="zoom-in">
-          <h2 class="section-heading text-uppercase">Gallery</h2>
+          <h2 class="site text-uppercase">Gallery</h2>
           <h3 class="section-subheading text-muted">sub header</h3>
         </div>
       </div>
       <div class="row">
-        <div class="col-lg-3 col-md-4 gallery mb-3">
+        <div class="col-lg-3 col-md-4 gallery mb-3" v-for="image in images" v-bind:key="image.id">
           <div class="gallery-item">
-            <a href="@/assets/img/gallery/1.jpg" data-fancybox="gallery" class="gallery-popup">
-              <img src="@/assets/img/gallery/1.jpg" alt />
-            </a>
-          </div>
-        </div>
-        <div class="col-lg-3 col-md-4 gallery mb-3">
-          <div class="gallery-item">
-            <a href="@/assets/img/gallery/1.jpg" data-fancybox="gallery" class="gallery-popup">
-              <img src="@/assets/img/gallery/1.jpg" alt />
-            </a>
-          </div>
-        </div>
-        <div class="col-lg-3 col-md-4 gallery mb-3">
-          <div class="gallery-item">
-            <a href="@/assets/img/gallery/1.jpg" data-fancybox="gallery" class="gallery-popup">
-              <img src="@/assets/img/gallery/1.jpg" alt />
-            </a>
-          </div>
-        </div>
-        <div class="col-lg-3 col-md-4 gallery mb-3">
-          <div class="gallery-item">
-            <a href="@/assets/img/gallery/1.jpg" data-fancybox="gallery" class="gallery-popup">
-              <img src="@/assets/img/gallery/1.jpg" alt />
-            </a>
-          </div>
-        </div>
-        <div class="col-lg-3 col-md-4 gallery mb-3">
-          <div class="gallery-item">
-            <a href="@/assets/img/gallery/1.jpg" data-fancybox="gallery" class="gallery-popup">
-              <img src="@/assets/img/gallery/1.jpg" alt />
-            </a>
-          </div>
-        </div>
-        <div class="col-lg-3 col-md-4 gallery mb-3">
-          <div class="gallery-item">
-            <a href="@/assets/img/gallery/1.jpg" data-fancybox="gallery" class="gallery-popup">
-              <img src="@/assets/img/gallery/1.jpg" alt />
-            </a>
-          </div>
-        </div>
-        <div class="col-lg-3 col-md-4 gallery mb-3">
-          <div class="gallery-item">
-            <a href="@/assets/img/gallery/1.jpg" data-fancybox="gallery" class="gallery-popup">
-              <img src="@/assets/img/gallery/1.jpg" alt />
-            </a>
-          </div>
-        </div>
-        <div class="col-lg-3 col-md-4 gallery mb-3">
-          <div class="gallery-item">
-            <a href="@/assets/img/gallery/1.jpg" data-fancybox="gallery" class="gallery-popup">
-              <img src="@/assets/img/gallery/1.jpg" alt />
+            <a :href="image.big_image" data-fancybox="gallery" class="gallery-popup">
+              <img :src="image.big_image" alt />
             </a>
           </div>
         </div>
@@ -68,3 +19,62 @@
     </div>
   </section>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      images: [
+        {
+          id: 1,
+          alt: "",
+          small_image: "assets/img/gallery/1.jpg",
+          big_image: "assets/img/gallery/1.jpg"
+        },
+        {
+          id: 2,
+          alt: "",
+          small_image: "assets/img/gallery/1.jpg",
+          big_image: "assets/img/gallery/1.jpg"
+        },
+        {
+          id: 3,
+          alt: "",
+          small_image: "/assets/img/gallery/1.jpg",
+          big_image: "/assets/img/gallery/1.jpg"
+        },
+        {
+          id: 4,
+          alt: "",
+          small_image: "/assets/img/gallery/1.jpg",
+          big_image: "/assets/img/gallery/1.jpg"
+        },
+        {
+          id: 5,
+          alt: "",
+          small_image: "/assets/img/gallery/1.jpg",
+          big_image: "/assets/img/gallery/1.jpg"
+        },
+        {
+          id: 6,
+          alt: "",
+          small_image: "/assets/img/gallery/1.jpg",
+          big_image: "/assets/img/gallery/1.jpg"
+        },
+        {
+          id: 7,
+          alt: "",
+          small_image: "/assets/img/gallery/1.jpg",
+          big_image: "/assets/img/gallery/1.jpg"
+        },
+        {
+          id: 8,
+          alt: "",
+          small_image: "/assets/img/gallery/1.jpg",
+          big_image: "/assets/img/gallery/1.jpg"
+        }
+      ]
+    };
+  }
+};
+</script>
