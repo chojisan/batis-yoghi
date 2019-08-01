92c2d7464672bc7b41f7514ed00824ac75b35ea1
Seigi Takada <seiji.takada@medixserve.com>
2019-08-01 14:11:50 +0800

    Updated navigation

--- src/views/components/HeaderNav.vue
+++ src/views/components/HeaderNav.vue
@@ -1,14 +1,27 @@
 <template>
   <base-nav
-    class="navbar-top navbar-horizontal navbar-dark fixed-top"
-    containerClasses="px-4 container"
+    class="navbar-mainnavbar-top navbar-horizontal navbar-dark fixed-top py-2"
+    containerClasses="container"
+    type
+    effect="light"
     expand
   >
     <router-link slot="brand" class="navbar-brand" to="/">
-      <img src="@/assets/img/logo/yoghi.png" class="img-logo" />
+      <img src="@/assets/img/logo/yoghi.png" />
     </router-link>
 
-    <ul class="nav ml-auto">
+    <div class="row" slot="content-header" slot-scope="{closeMenu}">
+      <div class="col-6 collapse-brand">
+        <a href="#">
+          <img src="@/assets/img/logo/yoghi.png" />
+        </a>
+      </div>
+      <div class="col-6 collapse-close">
+        <close-button @click="closeMenu"></close-button>
+      </div>
+    </div>
+
+    <ul class="nav navbar-nav navbar-nav-hover align-items-lg-center ml-auto">
       <li class="nav-item px-2">
         <router-link to="/" class="nav-link">HOME</router-link>
       </li>
@@ -30,3 +43,14 @@
     </ul>
   </base-nav>
 </template>
+
+<script>
+import CloseButton from "@/components/bootstrap/CloseButton";
+
+export default {
+  name: "navigation",
+  components: {
+    CloseButton
+  }
+};
+</script>
